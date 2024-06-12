8d6cf7c591481133db603a53fd6d054ceac87f02
Francesco Carbone <francesco.carbone@mermecgroup.com>
2024-06-12 15:06:00 +0200

    Projects

---
+++ src/Infrastructure/DeviceClient.cs
@@ -0,0 +1,20 @@
+﻿using IoT;
+
+namespace Infrastructure
+{
+    public class DeviceClient
+    {
+        private readonly DeviceService.DeviceServiceClient _client;
+
+        public DeviceClient(IoT.DeviceService.DeviceServiceClient client)
+        {
+            _client = client;
+        }
+
+        public async Task<object> GetDeivceAsync(CancellationToken cancellationToken)
+        {
+            var device = await _client.GetDeviceInformationAsync(new Google.Protobuf.WellKnownTypes.Empty(), cancellationToken: cancellationToken);
+            return device;
+        }
+    }
+}
